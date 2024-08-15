0ee1f43baebfe928d5cbeb48a11809be13b18477
princeyogesh <yogeshdeshpande79@gmail.com>
2024-08-15 16:37:01 +0530

    Update uploadmgr.go with parallel upload to different cloud storages

--- src/videoprocessor/uploadmgr.go
+++ src/videoprocessor/uploadmgr.go
@@ -1,6 +1,10 @@
 package main
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+	"sync"
+)
 
 /*
 objectProvider is just one of cloud object storage provider like AWS s3, azure blob, google obkect
@@ -10,26 +14,34 @@ chunks will always be stored in folder with name  sourcefile_
 
 func uploadChunkstoCloud(sourcefile string, objectProvider string) {
 	sourceFolder := sourcefile + "_"
-
+	var objectProviderCount = len(objectProvider) // change later when actually implemented
+	var wg sync.WaitGroup
+	wg.Add(objectProviderCount)
 	if strings.Compare(objectProvider, "S3") == 0 {
-		AWSS3Handler(sourceFolder)
+		go AWSS3Handler(sourceFolder, &wg)
 	}
 	if strings.Compare(objectProvider, "AzureBlob") == 0 {
-		AzureBlobHandler(sourceFolder)
+		go AzureBlobHandler(sourceFolder, &wg)
 	}
 	if strings.Compare(objectProvider, "GoogleStorage") == 0 {
-		GCPStorageHandler(sourceFolder)
+		go GCPStorageHandler(sourceFolder, &wg)
 	}
+
+	wg.Wait()
+	fmt.Println("uploaded to all requested objectstorages ", objectProvider)
 }
 
-func GCPStorageHandler(sourceFolder string) {
+func GCPStorageHandler(sourceFolder string, wg *sync.WaitGroup) {
+	defer wg.Done()
 	panic("unimplemented")
 }
 
-func AzureBlobHandler(sourceFolder string) {
+func AzureBlobHandler(sourceFolder string, wg *sync.WaitGroup) {
+	defer wg.Done()
 	panic("unimplemented")
 }
 
-func AWSS3Handler(sourceFolder string) {
+func AWSS3Handler(sourceFolder string, wg *sync.WaitGroup) {
+	defer wg.Done()
 	panic("unimplemented")
 }
