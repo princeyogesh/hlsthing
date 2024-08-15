d930719f08aedad6c5f2211ac33948087238db58
princeyogesh <yogeshdeshpande79@gmail.com>
2024-08-15 16:03:45 +0530

    Update main.go for creating blueprint for uploading to cloud

--- src/videoprocessor/main.go
+++ src/videoprocessor/main.go
@@ -5,11 +5,32 @@ import (
 	"fmt"
 )
 
+var storeToRemote *bool
+var CloudMode bool = false
+var CloudObjectProvider string = ""
+
 func main() {
 	inputFile := flag.String("filename", "/home/app/assets/BigBunny.mp4", "Please enter file name which you would like to proceed")
-
+	storeToRemote = flag.Bool("storeRemote", false, "used if we need to store .ts file remotely on some remote machine ")
 	fmt.Println("Processing the CLI input ", *inputFile)
 	fmt.Println("hello worlds")
 
+	//
 	breakdwonFile(*inputFile)
+	//
+	if CloudMode {
+		uploadChunkstoCloud(*inputFile, CloudObjectProvider)
+		docleanup()
+
+	} else if *storeToRemote {
+		docleanup()
+		panic("not able to send on any machine , not implemented ")
+	} else {
+		fmt.Println("no need to upload anywhere, please do manual cleanup")
+	}
+
+}
+
+func docleanup() {
+	fmt.Println("doing cleanup")
 }
